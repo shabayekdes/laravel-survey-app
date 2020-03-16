82e38a1cdb23bc0de742967d77dac601f7fadf19
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-03-16 22:31:44 +0200

    Added Questionnaire Module

---
+++ app/Http/Controllers/QuestionnaireController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Questionnaire;
+
+class QuestionnaireController extends Controller
+{
+    /**
+     * Questionnaire Controller Construct
+     *
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('questionnaire.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $questionnaire = auth()->user()->questionnaires()->create($this->validateRequest());
+
+        return redirect()->route('questionnaire.show', [
+            'questionnaire' => $questionnaire->id
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Questionnaire $questionnaire)
+    {
+        return view('questionnaire.show',compact('questionnaire'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    /**
+     * Request validate
+     *
+     * @return void
+     */
+    private function validateRequest()
+    {
+        return request()->validate([
+            'title' => 'required',
+            'purpose' => 'required'
+        ]);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
+use App\Models\Questionnaire;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -36,4 +37,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function questionnaires()
+    {
+        return $this->hasMany(Questionnaire::class);
+    }
 }
